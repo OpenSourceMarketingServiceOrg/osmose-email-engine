455941d8d03afe0bd36c98c4c005a7a5e4a15e43
Danjenator <danjenator@gmail.com>
2017-09-24 00:54:24 -0500

    aws is claiming the region isn't declared

--- src/index.js
+++ src/index.js
@@ -14,31 +14,31 @@ app.post('/sendEmail', function (req, res){
 //  console.log("req: ", req);
   console.log("req.body: ", req.body);
 
-  //  var params = {
-  //   Destination: {
-  //    BccAddresses: req.body.bbcList,
-  //    CcAddresses: req.body.ccList,
-  //    ToAddresses: req.body.toList
-  //   },
-  //   Message: {
-  //    Body: {
-  //     Html: {
-  //      Charset: "UTF-8",
-  //      Data: req.body.body
-  //     }
-  //    },
-  //    Subject: {
-  //     Charset: "UTF-8",
-  //     Data: req.body.subject
-  //    }
-  //   },
-  //   Source: source,
-  //   SourceArn: sourceArn
-  //  };
-  //  ses.sendEmail(params, function(err, data) {
-  //    if (err) console.log(err, err.stack); // an error occurred
-  //    else     console.log(data);           // successful response
-  //  });
+   var params = {
+    Destination: {
+     BccAddresses: req.body.bbcList,
+     CcAddresses: req.body.ccList,
+     ToAddresses: req.body.toList
+    },
+    Message: {
+     Body: {
+      Html: {
+       Charset: "UTF-8",
+       Data: req.body.body
+      }
+     },
+     Subject: {
+      Charset: "UTF-8",
+      Data: req.body.subject
+     }
+    },
+    Source: source,
+    SourceArn: sourceArn
+   };
+   ses.sendEmail(params, function(err, data) {
+     if (err) console.log(err, err.stack); // an error occurred
+     else     console.log(data);           // successful response
+   });
 
   res.send("success");
 });
