ad1b2aef041fc680f14ce384b1bb413e6cf3ed76
Danjenator <danjenator@gmail.com>
2017-09-23 22:22:30 -0500

    now running with express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Working");
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res){
+  res.send("hallo!");
+});
+
+app.listen(3001, function() {
+  console.log("Running on port 3001");
+});
