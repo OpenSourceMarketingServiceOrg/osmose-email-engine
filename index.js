3efaddc70c63bed5f1a29d3362b5062cc101c91d
Danjenator <danjenator@gmail.com>
2017-10-07 15:44:16 -0500

    added license

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const OsmoseEE = require('./dist/bundle');
+const osmose = require('./dist/bundle');
 
-console.log("engine: ", OsmoseEE);
+console.log("engine: ", osmose);
+let addresses = {
+  BccAddresses: [],
+  CcAddresses: [
+    "recipient3@example.com"
+  ],
+  ToAddresses: [
+    "recipient1@example.com",
+    "recipient2@example.com"
+  ]
+};
 
-OsmoseEE.testesFunction("DezzNutz");
+let email = {
+  subject: 'DezzNutz',
+  body: '<html><body><h1>Testes!</h1><p>workie?</p></body></html>'
+}
+
+let from = "danjenator";
+
+osmose.osmoseSendEmail(addresses, email, from);
--- src/osmose.ee.js
+++ src/osmose.ee.js
@@ -1,49 +1,20 @@
-//import AWS from 'aws-sdk';
-import params from './ee.create-params';
+import AWS from 'aws-sdk';
+import params from './ee.params';
 
-module.exports = class OsmoseEE {
+exports.osmoseSendEmail = (addresses, email, from)=>{
 
-  static testesFunction(input) {
+  // static sendEmail(input, addresses, email, from) {
 
-    // const ses = new AWS.SES({
-    //    apiVersion: '2010-12-01',
-    //    region: 'us-east-1'
-    // });
+    const ses = new AWS.SES({
+       apiVersion: '2010-12-01',
+       region: 'us-east-1'
+    });
 
-    console.log("input: ", input);
-    console.log("params", params.createParams());
+    console.log('params: ', params.create(addresses, email, from));
 
-    // let params = {
-    //   Destination: {
-    //     ToAddresses: [
-    //       'danjenator@gmail.com',
-    //       /* more items */
-    //     ]
-    //   },
-    //   Message: { /* required */
-    //     Body: { /* required */
-    //       Html: {
-    //         Data: '<html><body><h1>Testes!</h1><p>workie?</p></body></html>', /* required */
-    //         Charset: 'UTF-8'
-    //       }
-    //     },
-    //     Subject: { /* required */
-    //       Data: 'DezNutz', /* required */
-    //       Charset: 'UTF-8'
-    //     }
-    //   },
-    //   Source: 'danjenator@gmail.com', /* required */
-    //   Tags: [
-    //     {
-    //       Name: 'Id', /* required */
-    //       Value: '555-555' /* required */
-    //     },
-    //     /* more items */
-    //   ]
-    // };
-    // ses.sendEmail(params, function(err, data) {
+    // ses.sendEmail(params.create(addresses, email, from), function(err, data) {
     //   if (err) console.log(err, err.stack); // an error occurred
     //   else     console.log(data);           // successful response
     // });
-  }
+  //}
 }
